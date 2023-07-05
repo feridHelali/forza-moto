675ad2ce855b8c34620dda935feb0b33b7220dff
Ferid HELALI <helaliferid@gmail.com>
2023-07-05 20:44:23 +0100

    fix typo router

--- api/app.js
+++ api/app.js
@@ -1,11 +1,11 @@
 const express=require('express')
-const motorRouter = require('./routes/motorbike.routes')
+const motorbikeRouter = require('./routes/motorbike.routes')
 const cors=require('cors')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use('/motor',motorRouter)
+app.use('/motor',motorbikeRouter)
 
 
 
