617888a7d26fe50609f5ce2fce1f65b63548a28e
Ferid HELALI <helaliferid@gmail.com>
2023-07-13 10:57:15 +0100

    plan for CRUD Motorbike fron

--- app/src/components/Motorbike/MotorbikeList/MotorbikeList.jsx
+++ app/src/components/Motorbike/MotorbikeList/MotorbikeList.jsx
@@ -8,3 +8,10 @@ function MotorbikeList() {
 export default MotorbikeList
 
 //TODO: to implement MotorbikeList (urgent)
+// load All motorbikes from api
+// render motorbikes in table
+// implement Add New Motorbike --> navigate to AddMotorbike Form
+// Once successfully, redirect --> MotorbikeList
+// implement update motorbike --> navigate to update form (not yet implemented)
+// once updated --> redirect to MotorbikeList
+// bis : implement upload photo, cover in updateForm
