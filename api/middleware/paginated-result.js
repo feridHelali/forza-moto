06a2a7aa8eaf6c8b69b99d0414afa865e41783b4
Ferid HELALI <helaliferid@gmail.com>
2023-07-26 11:07:59 +0100

    fix search / query in api

--- api/middleware/paginated-result.js
+++ api/middleware/paginated-result.js
@@ -1,20 +1,22 @@
-function paginatedResult (model){
-    return async (req,res,next)=>{
+function paginatedResult(model) {
+    return async (req, res, next) => {
         let page = parseInt(req.query.page)
         let limit = parseInt(req.query.limit)
         let search = req.query.search;
-    
+
+        console.log(search)
+
         if (!page) page = 1;
         if (!limit) limit = 10;
-    
+
         const startIndex = (page - 1) * limit
         const endIndex = page * limit
 
         const count = await model.countDocuments().exec()
-    
+
         let info = {}
-    
-    
+
+
         if (endIndex < count) {
             info.next = {
                 page: page + 1,
@@ -22,24 +24,29 @@ function paginatedResult (model){
                 count: count
             }
         }
-    
+
         if (startIndex > 0) {
             info.previous = {
-                page: page-1,
+                page: page - 1,
                 limit: limit,
                 count: count
             }
         }
-    
+
         try {
-            const data = await model.find({$text: {$search: search}}).limit(limit).skip(startIndex).exec()
-            res.result={info,data},
-            next() 
+            let data;
+            if (search) {
+                data = await model.find({ $text: { $search: search } }).limit(limit).skip(startIndex).exec()
+            }else{
+                data = await model.find({}).limit(limit).skip(startIndex).exec()
+            }
+            res.result = { info, data },
+                next()
         } catch (error) {
-            res.status(500).json({error:error.message})
+            res.status(500).json({ error: error.message })
         }
     }
-    
+
 }
 
-module.exports=paginatedResult;
+module.exports = paginatedResult;
