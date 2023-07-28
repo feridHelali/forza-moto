e55975b0a7251a496c63062f6c045936a5455321
Ferid HELALI <helaliferid@gmail.com>
2023-07-28 19:16:44 +0100

    add footer to the app

--- app/src/components/layouts/Footer.jsx
+++ app/src/components/layouts/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Box, Container, Grid, Typography } from "@mui/material";
 
-function Footer() {
+export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box
+      sx={{
+        width: "100%",
+        height: "auto",
+        backgroundColor: "primary.main",
+        paddingTop: "1rem",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container direction="column" alignItems="center">
+          <Grid item xs={12}>
+            <Typography color="white" variant="h5">
+              Forza Space
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="white" variant="subtitle1">
+             &copy; Copywrite  {`${new Date().getFullYear()} |  Motorbikes | Marketplace | Social Inter-reactions`}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
