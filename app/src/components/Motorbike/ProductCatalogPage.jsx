6bf14d77bfb10c50c1b0d1e8d039a54525f9a67a
Ferid HELALI <helaliferid@gmail.com>
2023-07-28 21:30:50 +0100

    implement catalog Page

--- app/src/components/Motorbike/ProductCatalogPage.jsx
+++ app/src/components/Motorbike/ProductCatalogPage.jsx
@@ -1,11 +1,39 @@
-
+import { useEffect, useState } from "react";
+import api from "../../api/common.http";
+import './ProductCatalogPage.css'
+import getPhotoUrl from '../../utilities/getPhotoUrl'
+import { Typography } from "@mui/material";
 
 function ProductCatalogPage() {
+  const [motorbikes, setMotorbikes] = useState([]);
+
+  async function getMotorbikes() {
+    await api
+      .get(`/motor/all`)
+      .then((response) => response.data)
+      .then((data) => {
+        setMotorbikes(data.data);
+      })
+      .catch((error) => console.error(error.message));
+  }
+
+  useEffect(() => {
+    getMotorbikes();
+  }, []);
+
   return (
-    <div>ProductCatalogPage</div>
+    <div className="productCatalog">
+      {motorbikes?.map((motorbike) => (<div key={motorbike._id}>
+        <div className="motorbike__line">
+          <img src={getPhotoUrl(motorbike?.cover_url)} alt={motorbike.label} />
+          <Typography variant='h2' w={20} m={2} >{motorbike?.label}</Typography>
+          <Typography variant="h2" m={2}>{motorbike?.brand}</Typography>
+          <Typography variant="h2" m={2}>{motorbike?.price?.toFixed(3)}</Typography>
+        </div>
+      </div>))}
+    </div>
   )
 }
 
 export default ProductCatalogPage
 
-//TODO: to implement MotorbikeList (normal)
