1da5e8c492eed6bcb5014206fefa2f8fbea11d76
Ferid HELALI <helaliferid@gmail.com>
2023-07-28 19:19:15 +0100

    fix the style of footer

--- app/src/components/layouts/Footer.jsx
+++ app/src/components/layouts/Footer.jsx
@@ -3,12 +3,14 @@ import { Box, Container, Grid, Typography } from "@mui/material";
 export const Footer = () => {
   return (
     <Box
+      style={{ position: "fixed" }}
       sx={{
         width: "100%",
         height: "auto",
         backgroundColor: "primary.main",
         paddingTop: "1rem",
         paddingBottom: "1rem",
+        bottom:0
       }}
     >
       <Container maxWidth="lg">
@@ -20,7 +22,7 @@ export const Footer = () => {
           </Grid>
           <Grid item xs={12}>
             <Typography color="white" variant="subtitle1">
-             &copy; Copywrite  {`${new Date().getFullYear()} |  Motorbikes | Marketplace | Social Inter-reactions`}
+              &copy; Copywrite  {`${new Date().getFullYear()} |  Motorbikes | Marketplace | Social Inter-reactions`}
             </Typography>
           </Grid>
         </Grid>
