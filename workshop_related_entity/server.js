51e1a3630d0d227ad50174b71ff296fde03264b0
Ferid HELALI <helaliferid@gmail.com>
2023-07-31 10:40:51 +0100

    implment data validation of body in add Motorbike

--- workshop_related_entity/server.js
+++ workshop_related_entity/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const { body,validationResult, ValidationChain } = require('express-validator');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/my_db')
@@ -14,17 +15,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/my_db')
 
 
 const userSchema = new mongoose.Schema({
-    fullName: String,
-    address: String,
-    city: String
+    fullName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    city: { type: String }
 })
 
 const User = mongoose.model('User', userSchema)
 
 const orderSchema = new mongoose.Schema({
-    orderSate:{type:Date,default:new Date()},
+    orderSate: { type: Date, default: new Date() },
     user: { type: mongoose.Types.ObjectId, ref: 'User' },
-    details:[],
+    details: [],
     status: { type: String, default: 'Pending' }
 })
 
@@ -34,29 +43,51 @@ const app = express()
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.post('/user',async (req,res,next)=>{
-    const {fullName,address,city} = req.body
-    const result = await User.create({fullName,address,city})
+
+const validate = validations => {
+    return async (req, res, next) => {
+      for (let validation of validations) {
+        const result = await validation.run(req);
+        if (result.errors.length) break;
+      }
+  
+      const errors = validationResult(req);
+      if (errors.isEmpty()) {
+        return next();
+      }
+  
+      res.status(400).json({ errors: errors.array() });
+    };
+  };
+
+
+app.post('/user',validate([
+    body('fullName').isLength({min:5,max:50}),
+    body('address').optional(),
+    body('city').isLength({min:4,max:12})
+]) ,async (req, res, next) => {
+     const {fullName,address,city} = req.body
+    const result = await User.create({ fullName, address, city })
     res.status(201).json(result)
 
 })
 
-app.get('/user',async (req,res,next)=>{
-   
+app.get('/user', async (req, res, next) => {
+
     const result = await User.find({})
     res.status(201).json(result)
 
 })
-app.post('/order',async (req,res,next)=>{
-    const {user,details} = req.body
-    const result = await Order.create({user,details})
+app.post('/order', async (req, res, next) => {
+    const { user, details } = req.body
+    const result = await Order.create({ user, details })
     res.status(201).json(result)
 
 })
-app.get('/order',async (req,res,next)=>{
-    const result =await Order.find({}).populate({path:'user',select:'fullName address city'}) 
+app.get('/order', async (req, res, next) => {
+    const result = await Order.find({}).populate({ path: 'user', select: 'fullName address city' })
     res.status(200).json(result)
 })
 
